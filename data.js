67e0bb50a2c15d4a1d4b10ae488db0e063cee865
José Camelo Freitas <cmpjosf@outlook.com>
2020-03-24 13:29:39 -0300

    First commit

---
+++ data.js
@@ -0,0 +1,233 @@
+cities = [
+  {
+    "id": 1,
+    "name": "Maceió",
+    "coordinates": [
+      -9.641045,
+      -35.731096
+    ],
+    "suspects": 95,
+    "confirmed": 8,
+    "deaths": 0
+  },
+  {
+    "id": 2,
+    "name": "Arapiraca",
+    "coordinates": [
+      -9.751072,
+      -36.661224
+    ],
+    "suspects": 4,
+    "confirmed": 0,
+    "deaths": 0
+  },
+  {
+    "id": 3,
+    "name": "Ouro Branco",
+    "coordinates": [
+      -9.162164,
+      -37.358647
+    ],
+    "suspects": 2,
+    "confirmed": 0,
+    "deaths": 0
+  },
+  {
+    "id": 4,
+    "name": "Rio Largo",
+    "coordinates": [
+      -9.505435,
+      -35.817664
+    ],
+    "suspects": 2,
+    "confirmed": 0,
+    "deaths": 0
+  },
+  {
+    "id": 5,
+    "name": "Barra de Santo Antônio",
+    "coordinates": [
+      -9.404484,
+      -35.511819
+    ],
+    "suspects": 1,
+    "confirmed": 0,
+    "deaths": 0
+  },
+  {
+    "id": 6,
+    "name": "Barra de São Miguel",
+    "coordinates": [
+      -9.824005,
+      -35.887178
+    ],
+    "suspects": 1,
+    "confirmed": 0,
+    "deaths": 0
+  },
+  {
+    "id": 7,
+    "name": "São Miguel dos Campos",
+    "coordinates": [
+      -9.781726,
+      -36.102953
+    ],
+    "suspects": 1,
+    "confirmed": 0,
+    "deaths": 0
+  },
+  {
+    "id": 8,
+    "name": "Cajueiro",
+    "coordinates": [
+      -9.396621,
+      -36.154922
+    ],
+    "suspects": 1,
+    "confirmed": 0,
+    "deaths": 0
+  },
+  {
+    "id": 9,
+    "name": "Penedo",
+    "coordinates": [
+      -10.280024,
+      -36.556284
+    ],
+    "suspects": 1,
+    "confirmed": 0,
+    "deaths": 0
+  },
+  {
+    "id": 10,
+    "name": "Flexeiras",
+    "coordinates": [
+      -9.276373,
+      -35.726509
+    ],
+    "suspects": 1,
+    "confirmed": 0,
+    "deaths": 0
+  },
+  {
+    "id": 11,
+    "name": "Satuba",
+    "coordinates": [
+      -9.570656,
+      -35.825962
+    ],
+    "suspects": 1,
+    "confirmed": 0,
+    "deaths": 0
+  },
+  {
+    "id": 12,
+    "name": "Inhapi",
+    "coordinates": [
+      -9.222639,
+      -37.750531
+    ],
+    "suspects": 1,
+    "confirmed": 0,
+    "deaths": 0
+  },
+  {
+    "id": 13,
+    "name": "Maribondo",
+    "coordinates": [
+      -9.577649,
+      -36.306170
+    ],
+    "suspects": 1,
+    "confirmed": 0,
+    "deaths": 0
+  },
+  {
+    "id": 14,
+    "name": "Pilar",
+    "coordinates": [
+      -9.596389,
+      -35.958146
+    ],
+    "suspects": 1,
+    "confirmed": 0,
+    "deaths": 0
+  },
+  {
+    "id": 15,
+    "name": "Piaçabuçu",
+    "coordinates": [
+      -10.380358,
+      -36.400147
+    ],
+    "suspects": 1,
+    "confirmed": 0,
+    "deaths": 0
+  },
+  {
+    "id": 16,
+    "name": "Mata Grande",
+    "coordinates": [
+      -9.118482,
+      -37.731904
+    ],
+    "suspects": 1,
+    "confirmed": 0,
+    "deaths": 0
+  },
+  {
+    "id": 17,
+    "name": "Monteirópolis",
+    "coordinates": [
+      -9.603872,
+      -37.249668
+    ],
+    "suspects": 1,
+    "confirmed": 0,
+    "deaths": 0
+  },
+  {
+    "id": 18,
+    "name": "Coruripe",
+    "coordinates": [
+      -10.123703,
+      -36.175328
+    ],
+    "suspects": 1,
+    "confirmed": 0,
+    "deaths": 0
+  },
+  {
+    "id": 19,
+    "name": "Porto Real do Colégio",
+    "coordinates": [
+      -10.184518,
+      -36.834623
+    ],
+    "suspects": 1,
+    "confirmed": 0,
+    "deaths": 0
+  },
+  {
+    "id": 20,
+    "name": "Maragogi",
+    "coordinates": [
+      -9.008907,
+      -35.223233
+    ],
+    "suspects": 1,
+    "confirmed": 0,
+    "deaths": 0
+  },
+  {
+    "id": 21,
+    "name": "Marechal Deodoro",
+    "coordinates": [
+      -9.764584,
+      -35.852997
+    ],
+    "suspects": 1,
+    "confirmed": 0,
+    "deaths": 0
+  }
+];
---
+++ index.js
@@ -0,0 +1,256 @@
+const total = {
+  suspects: cities.reduce((prev, next) => prev + next.suspects, 0),
+  confirmed: cities.reduce((prev, next) => prev + next.confirmed, 0),
+  deaths: cities.reduce((prev, next) => prev + next.deaths, 0),
+};
+
+const getRadius = (quantity, total, maxSize, minSize = null) => {
+  minSize = minSize ? minSize : maxSize / 10;
+  const result = (quantity * maxSize) / total;
+
+  if (result === 0) {
+    return 0;
+  }
+
+  if (result < minSize) {
+    const increment = result / minSize;
+    return minSize + ((minSize * increment) * 5);
+  }
+
+  if (Number.isNaN(result)) {
+    return 0;
+  }
+
+  return result;
+}
+
+mapboxgl.accessToken = "pk.eyJ1Ijoiam9zZWZyZWl0dGFzIiwiYSI6ImNpcm1reHI4czAwY3FmZm02NHR3Z2N4YnEifQ.UQ9qtqcgob5PjZONlhK-zA";
+var map = new mapboxgl.Map({
+  container: "map",
+  style: "mapbox://styles/josefreittas/ck840k34g2skg1io71k2t5hkw",
+  center: [-36.680672, -9.574678],
+  maxZoom: 10,
+  minZoom: 7,
+  zoom: 8,
+});
+
+map.on("load", function () {
+  map.addSource("points", {
+    type: "geojson",
+    data: {
+      type: "FeatureCollection",
+      features: cities.map(city => {
+        return {
+          id: city.id,
+          type: "Feature",
+          geometry: {
+            type: "Point",
+            coordinates: city.coordinates.reverse(),
+          },
+          properties: {
+            title: city.name,
+            orgRadius: getRadius(city.suspects, total.suspects, 100),
+            redRadius: getRadius(city.confirmed, total.confirmed, 50),
+            blkRadius: getRadius(city.deaths, total.deaths, 25),
+            suspects: city.suspects,
+            confirmed: city.confirmed,
+            deaths: city.deaths,
+          }
+        };
+      })
+    }
+  });
+
+  map.addLayer({
+    id: "suspects",
+    source: "points",
+    type: "circle",
+    paint: {
+      "circle-radius": ["get", "orgRadius"],
+      "circle-color": [
+        "case",
+        [
+          "boolean",
+          ["feature-state", "hover"],
+          false,
+        ],
+        "rgb(250, 225, 175)",
+        "rgba(250, 225, 175, 0.75)",
+      ],
+      "circle-stroke-color": [
+        "case",
+        [
+          "boolean",
+          ["feature-state", "hover"],
+          false,
+        ],
+        "rgb(200, 150, 75)",
+        "rgba(200, 150, 75, 0.6)",
+      ],
+      "circle-stroke-width": 1,
+    },
+  });
+
+  map.addLayer({
+    id: "confirmed",
+    source: "points",
+    type: "circle",
+    paint: {
+      "circle-radius": ["get", "redRadius"],
+      "circle-color": [
+        "case",
+        [
+          "boolean",
+          ["feature-state", "hover"],
+          false,
+        ],
+        "rgb(250, 175, 175)",
+        "rgba(250, 175, 175, 0.5)",
+      ],
+      "circle-stroke-color": [
+        "case",
+        [
+          "boolean",
+          ["feature-state", "hover"],
+          false,
+        ],
+        "rgb(200, 125, 125)",
+        "rgba(200, 125, 125, 0.5)",
+      ],
+      "circle-stroke-width": 1,
+    },
+  });
+
+  map.addLayer({
+    id: "death",
+    source: "points",
+    type: "circle",
+    paint: {
+      "circle-radius": ["get", "blkRadius"],
+      "circle-color": "rgba(150, 150, 150, 0.6)",
+      "circle-stroke-color": [
+        "case",
+        [
+          "boolean",
+          ["feature-state", "hover"],
+          false,
+        ],
+        "rgb(110, 110, 110)",
+        "rgba(110, 110, 110, 0.6)",
+      ],
+      "circle-stroke-width": 1,
+    },
+  });
+
+  map.addLayer({
+    id: "hover-interact",
+    source: "points",
+    type: "circle",
+    paint: {
+      "circle-radius": ["get", "orgRadius"],
+      "circle-color": "rgba(250, 225, 175, 0)",
+      "circle-stroke-color": [
+        "case",
+        [
+          "boolean",
+          ["feature-state", "hover"],
+          false,
+        ],
+        "rgba(200, 150, 100, 1)",
+        "rgba(0, 0, 0, 0)",
+      ],
+      "circle-stroke-width": [
+        "case",
+        [
+          "boolean",
+          ["feature-state", "hover"],
+          false,
+        ],
+        5,
+        0,
+      ],
+    },
+  });
+
+});
+
+var popup = new mapboxgl.Popup({
+  closeButton: false,
+  closeOnClick: false
+});
+
+let pointId;
+
+map.on("mousemove", "hover-interact", (event) => {
+
+  map.getCanvas().style.cursor = "pointer";
+
+  var coordinates = event.features[0].geometry.coordinates.slice();
+  var description = `
+    <h1>${event.features[0].properties.title}</h1>
+    <div class="counter suspects">
+      <div>Suspeitos:</div>
+      <div>${event.features[0].properties.suspects}</div>
+    </div>
+    <div class="counter confirmed">
+      <div>Confirmados:</div>
+      <div>${event.features[0].properties.confirmed}</div>
+    </div>
+    <div class="counter deaths">
+      <div>Mortes:</div>
+      <div>${event.features[0].properties.deaths}</div>
+    </div>
+  `;
+
+  while (Math.abs(event.lngLat.lng - coordinates[0]) > 180) {
+    coordinates[0] += event.lngLat.lng > coordinates[0] ? 360 : -360;
+  }
+
+  // Populate the popup and set its coordinates
+  // based on the feature found.
+  popup
+    .setLngLat(coordinates)
+    .setHTML(description)
+    .addTo(map);
+
+  if (event.features.length > 0) {
+    if (pointId) {
+      map.removeFeatureState({
+        source: "points",
+        id: pointId
+      });
+    }
+
+    pointId = event.features[0].id;
+
+    map.setFeatureState(
+      {
+        source: "points",
+        id: pointId,
+      },
+      {
+        hover: true
+      }
+    );
+
+  }
+});
+
+map.on("mouseleave", "hover-interact", function () {
+  if (pointId) {
+    map.setFeatureState(
+      {
+        source: "points",
+        id: pointId
+      },
+      {
+        hover: false
+      }
+    );
+  }
+
+  pointId = null;
+
+  popup.remove();
+  map.getCanvas().style.cursor = "";
+});
