91abd29db66882d0166b9bdb40231488c7d58868
José Camelo Freitas <cmpjosf@outlook.com>
2020-03-26 01:19:41 -0300

    refactor: cleanup in the project structure + readme

---
+++ README.md
@@ -0,0 +1,5 @@
+## Mapa de acompanhamento de casos do COVID-19 no estado de Alagoas
+
+Atualizado com dados da [Secretaria de Estado da Saúde de Alagoas](http://www.saude.al.gov.br) e da [Prefeitura de arapiraca](http://web.arapiraca.al.gov.br).
+
+Como o fornecimento de dados não é padronizado, e muita vezes é dado até por redes sociais, o processo de atualização é manual.
--- data.js
+++ data.js
@@ -1,148 +1,162 @@
-cities = [
-  {
-    coordinates: [-9.644031, -35.720957],
-    suspects: 145,
-    confirmed: 10,
-    name: "Maceió",
-  },
-  {
-    coordinates: [-9.749389, -36.667732],
-    suspects: 16,
-    name: "Arapiraca",
-  },
-  {
-    coordinates: [-9.781355, -36.351110],
-    suspects: 6,
-    name: "Campo Alegre",
-  },
-  {
-    coordinates: [-9.010249, -35.224591],
-    suspects: 4,
-    name: "Maragogi",
-  },
-  {
-    coordinates: [-9.396016, -36.156007],
-    suspects: 2,
-    name: "Cajueiro",
-  },
-  {
-    coordinates: [-9.275353, -35.724899],
-    suspects: 2,
-    name: "Flexeiras",
-  },
-  {
-    coordinates: [-9.230914, -37.350396],
-    suspects: 2,
-    name: "Maravilha",
-  },
-  {
-    coordinates: [-9.404533, -35.513499],
-    suspects: 1,
-    name: "Barra de Santo Antônio",
-  },
-  {
-    coordinates: [-9.842640, -35.903430],
-    suspects: 2,
-    name: "Barra de São Miguel",
-  },
-  {
-    coordinates: [-9.570445, -36.493012],
-    suspects: 1,
-    name: "Belém",
-  },
-  {
-    coordinates: [-8.912571, -35.722496],
-    suspects: 1,
-    name: "Colônia Leopoldina",
-  },
-  {
-    coordinates: [-8.974415, -35.931813],
-    suspects: 1,
-    name: "Ibateguara",
-  },
-  {
-    coordinates: [-9.515122, -36.993557],
-    suspects: 1,
-    name: "Major Izidoro",
-  },
-  {
-    coordinates: [-9.577329, -36.307092],
-    suspects: 1,
-    name: "Maribondo",
-  },
-  {
-    coordinates: [-9.118287, -37.730786],
-    suspects: 1,
-    name: "Mata Grande",
-  },
-  {
-    coordinates: [-10.185278, -36.834924],
-    confirmed: 1,
-    name: "Porto Real do Colégio",
-  },
-  {
-    coordinates: [-9.603816, -37.249569],
-    suspects: 2,
-    name: "Monteirópolis",
-  },
-  {
-    coordinates: [-9.532001, -37.295814],
-    suspects: 1,
-    name: "Olho d’Água das Flores",
-  },
-  {
-    coordinates: [-9.162719, -37.359068],
-    suspects: 2,
-    name: "Ouro Branco",
-  },
-  {
-    coordinates: [-9.413621, -36.633998],
-    suspects: 3,
-    name: "Palmeira dos Índios",
-  },
-  {
-    coordinates: [-9.463793, -35.553035],
-    suspects: 1,
-    name: "Paripueira",
-  },
-  {
-    coordinates: [-10.396530, -36.423365],
-    suspects: 1,
-    name: "Piaçabuçu",
-  },
-  {
-    coordinates: [-9.606529, -37.767320],
-    suspects: 1,
-    name: "Piranhas",
-  },
-  {
-    coordinates: [-9.306580, -37.286316],
-    suspects: 4,
-    name: "Poço das Trincheiras",
-  },
-  {
-    coordinates: [-9.503844, -35.818990],
-    suspects: 5,
-    name: "Rio Largo",
-  },
-  {
-    coordinates: [-9.368745, -37.249466],
-    suspects: 4,
-    name: "Santana do Ipanema",
-  },
-  {
-    coordinates: [-9.557613, -37.381718],
-    suspects: 1,
-    name: "São José da Tapera",
-  },
-  {
-    coordinates: [-9.569310, -35.828192],
-    suspects: 1,
-    name: "Satuba",
-  },
-  {
-    coordinates: [-9.908589, -36.365970],
-    suspects: 1,
-    name: "Teotonio Vilela",
-  },
-];
+const data = {
+  lastUpdate: "25/03/2020",
+  sources: [
+    {
+      title: "Prefeitura de Arapiraca",
+      url: "https://www.instagram.com/prefeituradearapiraca/"
+    },
+    {
+      title: "Governo de Alagoas",
+      url: "http://www.saude.al.gov.br/2020/03/25/boletim-covid-19-25-03-20-alagoas-tem-11-casos-confirmados/"
+    }
+  ],
+  center: [-36.680672, -9.574678],
+  cities: [
+    {
+      coordinates: [-9.644031, -35.720957],
+      suspects: 145,
+      confirmed: 10,
+      name: "Maceió",
+    },
+    {
+      coordinates: [-9.749389, -36.667732],
+      suspects: 16,
+      name: "Arapiraca",
+    },
+    {
+      coordinates: [-9.781355, -36.351110],
+      suspects: 6,
+      name: "Campo Alegre",
+    },
+    {
+      coordinates: [-9.010249, -35.224591],
+      suspects: 4,
+      name: "Maragogi",
+    },
+    {
+      coordinates: [-9.396016, -36.156007],
+      suspects: 2,
+      name: "Cajueiro",
+    },
+    {
+      coordinates: [-9.275353, -35.724899],
+      suspects: 2,
+      name: "Flexeiras",
+    },
+    {
+      coordinates: [-9.230914, -37.350396],
+      suspects: 2,
+      name: "Maravilha",
+    },
+    {
+      coordinates: [-9.404533, -35.513499],
+      suspects: 1,
+      name: "Barra de Santo Antônio",
+    },
+    {
+      coordinates: [-9.842640, -35.903430],
+      suspects: 2,
+      name: "Barra de São Miguel",
+    },
+    {
+      coordinates: [-9.570445, -36.493012],
+      suspects: 1,
+      name: "Belém",
+    },
+    {
+      coordinates: [-8.912571, -35.722496],
+      suspects: 1,
+      name: "Colônia Leopoldina",
+    },
+    {
+      coordinates: [-8.974415, -35.931813],
+      suspects: 1,
+      name: "Ibateguara",
+    },
+    {
+      coordinates: [-9.515122, -36.993557],
+      suspects: 1,
+      name: "Major Izidoro",
+    },
+    {
+      coordinates: [-9.577329, -36.307092],
+      suspects: 1,
+      name: "Maribondo",
+    },
+    {
+      coordinates: [-9.118287, -37.730786],
+      suspects: 1,
+      name: "Mata Grande",
+    },
+    {
+      coordinates: [-10.185278, -36.834924],
+      confirmed: 1,
+      name: "Porto Real do Colégio",
+    },
+    {
+      coordinates: [-9.603816, -37.249569],
+      suspects: 2,
+      name: "Monteirópolis",
+    },
+    {
+      coordinates: [-9.532001, -37.295814],
+      suspects: 1,
+      name: "Olho d’Água das Flores",
+    },
+    {
+      coordinates: [-9.162719, -37.359068],
+      suspects: 2,
+      name: "Ouro Branco",
+    },
+    {
+      coordinates: [-9.413621, -36.633998],
+      suspects: 3,
+      name: "Palmeira dos Índios",
+    },
+    {
+      coordinates: [-9.463793, -35.553035],
+      suspects: 1,
+      name: "Paripueira",
+    },
+    {
+      coordinates: [-10.396530, -36.423365],
+      suspects: 1,
+      name: "Piaçabuçu",
+    },
+    {
+      coordinates: [-9.606529, -37.767320],
+      suspects: 1,
+      name: "Piranhas",
+    },
+    {
+      coordinates: [-9.306580, -37.286316],
+      suspects: 4,
+      name: "Poço das Trincheiras",
+    },
+    {
+      coordinates: [-9.503844, -35.818990],
+      suspects: 5,
+      name: "Rio Largo",
+    },
+    {
+      coordinates: [-9.368745, -37.249466],
+      suspects: 4,
+      name: "Santana do Ipanema",
+    },
+    {
+      coordinates: [-9.557613, -37.381718],
+      suspects: 1,
+      name: "São José da Tapera",
+    },
+    {
+      coordinates: [-9.569310, -35.828192],
+      suspects: 1,
+      name: "Satuba",
+    },
+    {
+      coordinates: [-9.908589, -36.365970],
+      suspects: 1,
+      name: "Teotonio Vilela",
+    },
+  ],
+};
