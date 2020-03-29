bf73dc52bdc7a505434047a760ddefd2ac3cf3cf
José Camelo Freitas <cmpjosf@outlook.com>
2020-03-29 20:09:44 -0300

    improvement: update data (03/29/2020)

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 const data = {
-  lastUpdate: "28/03/2020",
+  lastUpdate: "29/03/2020",
   sources: [
     {
       title: "Prefeitura de Arapiraca",
@@ -13,23 +13,7 @@ const data = {
   center: [-36.680672, -9.574678],
   cities: [
     {
-      confirmed: 13,
-      suspects: 192,
-      name: "Maceió",
-      coordinates: [-9.644031, -35.720957],
-    },
-    {
-      suspects: 30,
-      name: "Arapiraca",
-      coordinates: [-9.749389, -36.667732],
-    },
-    {
-      suspects: 15,
-      name: "Boca da Mata",
-      coordinates: [-9.643075, -36.214381],
-    },
-    {
-      suspects: 1,
+      suspects: 2,
       name: "Barra de Santo Antônio",
       coordinates: [-9.404533, -35.513499],
     },
@@ -40,43 +24,8 @@ const data = {
     },
     {
       suspects: 1,
-      name: "Belém",
-      coordinates: [-9.570445, -36.493012],
-    },
-    {
-      suspects: 2,
-      name: "Cajueiro",
-      coordinates: [-9.396016, -36.156007],
-    },
-    {
-      suspects: 6,
-      name: "Campo Alegre",
-      coordinates: [-9.781355, -36.351110],
-    },
-    {
-      suspects: 1,
-      name: "Colônia Leopoldina",
-      coordinates: [-8.912571, -35.722496],
-    },
-    {
-      suspects: 1,
       name: "Coqueiro Seco",
       coordinates: [-9.635525, -35.798495],
-    },
-    {
-      suspects: 1,
-      name: "Craíbas",
-      coordinates: [-9.617434, -36.767749],
-    },
-    {
-      suspects: 1,
-      name: "Delmiro Gouveia",
-      coordinates: [-9.384489, -38.001509],
-    },
-    {
-      suspects: 1,
-      name: "Feira Grande",
-      coordinates: [-9.899160, -36.677867],
     },
     {
       suspects: 2,
@@ -84,44 +33,10 @@ const data = {
       coordinates: [-9.275353, -35.724899],
     },
     {
-      suspects: 1,
-      name: "Ibateguara",
-      coordinates: [-8.974415, -35.931813],
-    },
-    {
-      suspects: 1,
-      name: "Igaci",
-      coordinates: [-9.537429, -36.630650],
-    },
-    {
-      suspects: 1,
-      name: "Jacuípe",
-      coordinates: [-8.840652, -35.460810],
-    },
-    {
-      suspects: 2,
-      name: "Jequiá da Praia",
-      coordinates: [-10.011523, -36.024980],
-    },
-    {
-      suspects: 1,
-      name: "Jundiá",
-      coordinates: [-8.941158, -35.555104],
-    },
-    {
-      suspects: 1,
-      name: "Major Izidoro",
-      coordinates: [-9.531568, -36.988572],
-    },
-    {
-      suspects: 5,
-      name: "Maragogi",
-      coordinates: [-9.010249, -35.224591],
-    },
-    {
-      suspects: 3,
-      name: "Maravilha",
-      coordinates: [-9.230914, -37.350396],
+      suspects: 183,
+      confirmed: 16,
+      name: "Maceió",
+      coordinates: [-9.644031, -35.720957],
     },
     {
       suspects: 3,
@@ -129,89 +44,9 @@ const data = {
       coordinates: [-9.763292, -35.852467],
     },
     {
-      suspects: 1,
-      name: "Maribondo",
-      coordinates: [-9.577329, -36.307092],
-    },
-    {
-      suspects: 2,
-      name: "Mata Grande",
-      coordinates: [-9.118287, -37.730786],
-    },
-    {
-      suspects: 2,
-      name: "Monteirópolis",
-      coordinates: [-9.603816, -37.249569],
-    },
-    {
-      suspects: 2,
-      name: "Olho d’Água das Flores",
-      coordinates: [-9.532001, -37.295814],
-    },
-    {
-      suspects: 1,
-      name: "Olho D’Água Grande",
-      coordinates: [-10.057884, -36.813196],
-    },
-    {
-      suspects: 2,
-      name: "Ouro Branco",
-      coordinates: [-9.162719, -37.359068],
-    },
-    {
-      suspects: 3,
-      name: "Palmeira dos Índios",
-      coordinates: [-9.413621, -36.633998],
-    },
-    {
-      suspects: 1,
-      name: "Paripueira",
-      coordinates: [-9.463793, -35.553035],
-    },
-    {
-      suspects: 1,
-      name: "Penedo",
-      coordinates: [-10.283255, -36.562289],
-    },
-    {
-      suspects: 1,
-      name: "Piaçabuçu",
-      coordinates: [-10.396530, -36.423365],
-    },
-    {
-      suspects: 2,
-      name: "Piranhas",
-      coordinates: [-9.606529, -37.767320],
-    },
-    {
-      suspects: 2,
-      name: "Poço das Trincheiras",
-      coordinates: [-9.306580, -37.286316],
-    },
-    {
-      suspects: 6,
+      suspects: 5,
       name: "Rio Largo",
       coordinates: [-9.503844, -35.818990],
-    },
-    {
-      suspects: 5,
-      name: "Santana do Ipanema",
-      coordinates: [-9.368745, -37.249466],
-    },
-    {
-      suspects: 1,
-      name: "São José da Tapera",
-      coordinates: [-9.557613, -37.381718],
-    },
-    {
-      suspects: 1,
-      name: "São Bras",
-      coordinates: [-10.120530, -36.895705],
-    },
-    {
-      suspects: 1,
-      name: "São Miguel dos Campos",
-      coordinates: [-9.784275, -36.103027],
     },
     {
       suspects: 3,
@@ -220,13 +55,163 @@ const data = {
     },
     {
       suspects: 1,
-      name: "Teotonio Viela",
-      coordinates: [-9.908589, -36.365970],
+      name: "Jacuípe",
+      coordinates: [-8.840652, -35.460810],
+    },
+    {
+      suspects: 5,
+      name: "Maragogi",
+      coordinates: [-9.010249, -35.224591],
+    },
+    {
+      suspects: 1,
+      name: "São Luís do Quitunde",
+      coordinates: [-9.315124, -35.565010],
+    },
+    {
+      suspects: 1,
+      name: "Colônia Leopoldina",
+      coordinates: [-8.912571, -35.722496],
+    },
+    {
+      suspects: 1,
+      name: "Ibateguara",
+      coordinates: [-8.974415, -35.931813],
+    },
+    {
+      suspects: 1,
+      name: "Murici",
+      coordinates: [-9.310076, -35.942315],
     },
     {
       suspects: 2,
       name: "União dos Palmares",
       coordinates: [-9.159061, -36.025219],
+    },
+    {
+      suspects: 2,
+      name: "Cajueiro",
+      coordinates: [-9.396016, -36.156007],
+    },
+    {
+      suspects: 15,
+      name: "Boca da Mata",
+      coordinates: [-9.643075, -36.214381],
+    },
+    {
+      suspects: 8,
+      name: "Campo Alegre",
+      coordinates: [-9.781355, -36.351110],
+    },
+    {
+      suspects: 1,
+      name: "Teotônio Vilela",
+      coordinates: [-9.909087, -36.357519],
+    },
+    {
+      suspects: 2,
+      name: "Jequiá da Praia",
+      coordinates: [-10.011523, -36.024980],
+    },
+    {
+      suspects: 1,
+      name: "Piaçabuçu",
+      coordinates: [-10.396530, -36.423365],
+    },
+    {
+      suspects: 1,
+      name: "São Brás",
+      coordinates: [-10.120403, -36.898923],
+    },
+    {
+      suspects: 19,
+      name: "Arapiraca",
+      coordinates: [-9.749389, -36.667732],
+    },
+    {
+      suspects: 1,
+      name: "Craíbas",
+      coordinates: [-9.617434, -36.767749],
+    },
+    {
+      suspects: 1,
+      name: "Feira Grande",
+      coordinates: [-9.899160, -36.677867],
+    },
+    {
+      suspects: 1,
+      name: "Limoeiro de Anadia",
+      coordinates: [-9.739527, -36.503312],
+    },
+    {
+      suspects: 1,
+      name: "Olho D'Água Grande",
+      coordinates: [-10.057884, -36.813196],
+    },
+    {
+      suspects: 2,
+      name: "Belém",
+      coordinates: [-9.570445, -36.493012],
+    },
+    {
+      suspects: 1,
+      name: "Igaci",
+      coordinates: [-9.537429, -36.630650],
+    },
+    {
+      suspects: 1,
+      name: "Maribondo",
+      coordinates: [-9.577329, -36.307092],
+    },
+    {
+      suspects: 3,
+      name: "Palmeira dos Índios",
+      coordinates: [-9.413621, -36.633998],
+    },
+    {
+      suspects: 3,
+      name: "Maravilha",
+      coordinates: [-9.230914, -37.350396],
+    },
+    {
+      suspects: 2,
+      name: "Monteirópolis",
+      coordinates: [-9.603816, -37.249569],
+    },
+    {
+      suspects: 2,
+      name: "Olho D'Água das Flores",
+      coordinates: [-9.532001, -37.295814],
+    },
+    {
+      suspects: 2,
+      name: "Ouro Branco",
+      coordinates: [-9.162719, -37.359068],
+    },
+    {
+      suspects: 2,
+      name: "Poço das Trincheiras",
+      coordinates: [-9.306580, -37.286316],
+    },
+    {
+      suspects: 6,
+      name: "Santana do Ipanema",
+      coordinates: [-9.368745, -37.249466],
+    },
+    {
+      suspects: 1,
+      name: "Delmiro Gouveia",
+      coordinates: [-9.384489, -38.001509],
+    },
+    {
+      suspects: 2,
+      name: "Mata Grande",
+      coordinates: [-9.118287, -37.730786],
+    },
+    {
+      suspects: 2,
+      name: "Piranhas",
+      coordinates: [-9.606529, -37.767320],
     },
     {
       confirmed: 1,
