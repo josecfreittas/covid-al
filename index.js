cb8ac93767612a6451604212bb998348dd8710f8
José Camelo Freitas <cmpjosf@outlook.com>
2020-04-05 01:16:53 -0300

    improvement: the app data is now a json file

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 import { loadMap } from "./lib/map.js";
 import mapboxToken from "./mapboxToken.js";
 
-loadMap({
-  token: mapboxToken,
-  style: "mapbox://styles/josefreittas/ck840k34g2skg1io71k2t5hkw",
-  center: data.center,
-  data: data.cities,
-});
+const start = async () => {
 
-const infoEl = document.getElementById("info").innerHTML = `
-  <a href="https://github.com/josecfreittas/covid-al">Source code ♥</a>
-  <span> | </span>
-  <span>Atualizado em ${data.lastUpdate}</span>
-  <span> | </span>
-  <span>Fontes: </span>
-  ${data.sources.map((source) => ` <a target="_blank" href="${source.url}">${source.title}</a>`)}
-`;
+  const data = await fetch('./data.json').then(data => data.json());
+
+  loadMap({
+    token: mapboxToken,
+    style: "mapbox://styles/josefreittas/ck840k34g2skg1io71k2t5hkw",
+    center: data.center,
+    data: data.cities,
+  });
+
+  const infoEl = document.getElementById("info").innerHTML = `
+    <a href="https://github.com/josecfreittas/covid-al">Source code ♥</a>
+    <span> | </span>
+    <span>Atualizado em ${data.lastUpdate}</span>
+    <span> | </span>
+    <span>Fontes: </span>
+    ${data.sources.map((source) => ` <a target="_blank" href="${source.url}">${source.title}</a>`)}
+  `;
+};
+
+start();
